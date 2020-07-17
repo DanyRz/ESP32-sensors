1047eadedd18999ebd303905ff7e7c6c8d7567a9
DanyRz <68063686+DanyRz@users.noreply.github.com>
2020-07-17 14:17:30 +0300

    Update sender_test.ino

--- connection_test/sender_test.ino
+++ connection_test/sender_test.ino
@@ -1,18 +1,28 @@
 #include <SPI.h>
 #include <LoRa.h>
-
+/* */
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
+// #define BAND 866E6
 
 int counter = 0;
 
 void setup() {
   
-  Serial.begin(115200);
- 
+  Serial.begin(115200); 
   Serial.println("LoRa Sender");
-
-  LoRa.begin(866E6);
-   
-  LoRa.setSyncWord(0xF3);
+  SPI.begin(SCK, MISO, MOSI, SS);  
+  LoRa.setPins(SS, RST, DIO0);  
+  LoRa.begin(866E6); 
+  LoRa.setSyncWord(0xF2);
+  if (!LoRa.begin(866E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }  
   Serial.println("LoRa Initializing OK!");
 }
 
