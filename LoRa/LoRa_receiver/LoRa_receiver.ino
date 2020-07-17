56af3a4aefe44e11bb55ee90c3758eb733b15d6e
DanyRz <68063686+DanyRz@users.noreply.github.com>
2020-07-17 14:19:31 +0300

    Update LoRa_receiver.ino

--- LoRa/LoRa_receiver/LoRa_receiver.ino
+++ LoRa/LoRa_receiver/LoRa_receiver.ino
@@ -5,13 +5,25 @@
 #include <SPI.h>
 #include <LoRa.h>
 
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
+
 void setup() {
   Serial.begin(115200);
-  while (!LoRa.begin(866E6)) {
-    Serial.println();
-    delay(500);
-  }
+  Serial.println("LoRa Receiver");
+  SPI.begin(SCK, MISO, MOSI, SS);  
+  LoRa.setPins(SS, RST, DIO0);  
+  LoRa.begin(866E6); 
   LoRa.setSyncWord(0xF5);
+  if (!LoRa.begin(866E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }  
+  Serial.println("LoRa Initializing OK!");
 }
 
 void loop() {
