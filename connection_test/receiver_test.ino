4e4305cb8f48456fd2572881953e52df4f495a7b
DanyRz <68063686+DanyRz@users.noreply.github.com>
2020-07-17 14:16:52 +0300

    Update receiver_test.ino

--- connection_test/receiver_test.ino
+++ connection_test/receiver_test.ino
@@ -1,28 +1,43 @@
 #include <SPI.h>
 #include <LoRa.h>
 
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
+// #define BAND 866E6
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial);
   Serial.println("LoRa Receiver");
-  LoRa.begin(866E6);
-  LoRa.setSyncWord(0xF3);
+  SPI.begin(SCK, MISO, MOSI, SS);
+  LoRa.setPins(SS, RST, DIO0); 
+  LoRa.begin(866E6); 
+  LoRa.setSyncWord(0xF2);
+  if (!LoRa.begin(866E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }  
   Serial.println("LoRa Initializing OK!");
 }
 
 void loop() {
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    Serial.print("Received packet '");
-
+ 
+  
     while (LoRa.available()) {
+      
+      int packetSize = LoRa.parsePacket();
       String LoRaData = LoRa.readString();
+      Serial.print("Received packet '");
       Serial.print(LoRaData); 
+
+      Serial.print("' with RSSI ");
+      Serial.println(LoRa.packetRssi());
     }
 
+      
   
-    Serial.print("' with RSSI ");
-    Serial.println(LoRa.packetRssi());
-  }
+  
 }
