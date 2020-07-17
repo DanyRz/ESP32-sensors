0a18ca3670168eac988bc86b6d38ef7833101bba
DanyRz <68063686+DanyRz@users.noreply.github.com>
2020-07-17 14:18:55 +0300

    Update LoRa_sender.ino

--- LoRa/LoRa_sender/LoRa_sender.ino
+++ LoRa/LoRa_sender/LoRa_sender.ino
@@ -4,9 +4,16 @@
 
 #include <Wire.h>
 #include <Adafruit_BMP085.h>
-#include "SparkFunHTU21D.h"
+#include <SparkFunHTU21D.h>
 #include <SPI.h>
 #include <LoRa.h>
+
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
 
 HTU21D myHumidity;
 Adafruit_BMP085 bmp;
@@ -14,12 +21,17 @@ Adafruit_BMP085 bmp;
 void setup() {
   Serial.begin(115200);
   myHumidity.begin();
-  bmp.begin();
-  while (!LoRa.begin(866E6)) {
-  Serial.println();
-  delay(500);
-}
+  bmp.begin(); 
+  Serial.println("LoRa Sender");
+  SPI.begin(SCK, MISO, MOSI, SS);  
+  LoRa.setPins(SS, RST, DIO0);  
+  LoRa.begin(866E6); 
   LoRa.setSyncWord(0xF5);
+  if (!LoRa.begin(866E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }  
+  Serial.println("LoRa Initializing OK!");
 }
   
 void loop() {
@@ -46,7 +58,7 @@ void loop() {
     LoRa.print(humd);
     LoRa.print("% ");
     LoRa.endPacket();
-    
+    Serial.println("Packet sent");
     delay(5000);
    
 }
